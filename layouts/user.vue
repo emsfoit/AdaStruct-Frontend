209f01a45725f9ee3496e63c43875067c5264494
Moe <emsfoit@gmail.com>
2022-03-09 16:23:21 +0100

    add user layout

---
+++ layouts/user.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-wrapper" v-show="isLoaded">
+    <!-- <navigation /> -->
+    <Nuxt />
+    <!-- <error v-else />
+    <app-footer /> -->
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "users_layout",
+  middleware: ["user"],
+  data() {
+    return {
+      isLoaded: false,
+      error: false,
+    };
+  },
+  computed: {
+    ...mapGetters({
+      user: "auth/user",
+    }),
+  },
+  mounted() {
+    this.isLoaded = true;
+  },
+};
+</script>
