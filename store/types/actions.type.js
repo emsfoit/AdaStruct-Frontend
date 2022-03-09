4342f5fe8f16ac2b89071ec34fcf04b4bfa522cd
Moe <emsfoit@gmail.com>
2022-03-09 16:15:54 +0100

    add initial store types

---
+++ store/types/actions.type.js
@@ -0,0 +1,18 @@
+// BASE
+export const CREATE = "create";
+export const DESTROY = "delete";
+export const FETCH_ALL = "fetch_all";
+export const FETCH_ONE = "fetch_one";
+export const UPDATE = "UPDATE";
+export const PATCH = "patch";
+
+export const CHECK_AUTH = "fetchAccessToken";
+export const LOGIN = "login";
+export const LOGOUT = "logout";
+export const RESET_PASSWORD = "reset-password";
+export const UPDATE_PASSWORD = "update-password";
+export const CHANGE_PASSWORD = "change-password";
+export const GET_USER_PROFILE = "get-user-profile";
+
+export const SIGNUP = "sign_up";
+export const VALIDATE = "validate";
---
+++ store/types/mutations.type.js
@@ -0,0 +1,23 @@
+export const SET_META = "set_meta";
+export const SET_PAGE = "set_page";
+export const SET_SEARCH = "set_search";
+export const SET_LOADING = "set_loading";
+export const SET_QUERIES = "set_queries";
+
+export const SET_USER = "setUser";
+export const SET_AUTH = "setAuth";
+export const PURGE_AUTH = "logOut";
+export const RESET_STATE = "resetState";
+export const SET_ERROR = "setError";
+export const SET_USER_TYPE = "setUserType";
+export const INIT = "init";
+
+// AUTH
+export const TOKEN = "token";
+export const REFRECH_TOKEN = "refrech_token";
+export const USER = "user";
+export const USER_TYPE = "user_type";
+// ALERT
+export const SET_ALERT = "set_alert";
+export const SET_COUNT_DOWN_ALERT = "set_count_down_alert";
+export const RESET_ALERT = "reset_alert";
