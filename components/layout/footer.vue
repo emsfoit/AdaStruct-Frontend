6fff5b17ff7d9611938f2f3bdb35c1ee0e2243e0
Moe <emsfoit@gmail.com>
2022-03-09 16:20:02 +0100

    default layout

---
+++ components/layout/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Footer Section -- Start -->
+  <footer class="footer">
+    <div class="row">
+      <img src="@/assets/img/footer.jpg" class="img" style="width: 100%" />
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-show="isLoaded">
+      <layout-navbar />
+      <Nuxt />
+      <layout-footer />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ["auth_checker"],
+  data() {
+    return {
+      isLoaded: false,
+    };
+  },
+  mounted() {
+    this.isLoaded = true;
+  },
+};
+</script>
