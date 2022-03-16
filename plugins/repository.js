a2c10ac490889f135327da7bfe26a55bc3bd99df
Moe <emsfoit@gmail.com>
2022-03-16 15:24:43 +0100

    inject api services in ctx

--- plugins/repository.js
+++ plugins/repository.js
@@ -1,7 +1,11 @@
 import AuthService from "../api/services/AuthService";
+import ProjectsService from "../api/services/ProjectsService";
+import DatasetsService from "../api/services/DatasetsService";
 
 export default (ctx, inject) => {
   inject("AuthService", AuthService(ctx.$api));
+  inject("ProjectsService", ProjectsService(ctx.$api));
+  inject("DatasetsService", DatasetsService(ctx.$api));
 
   // Add new api services here
 };
