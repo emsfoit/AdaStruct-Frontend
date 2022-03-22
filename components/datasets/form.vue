061b3a0b51ed6ba05e2ebeb51612dd896ea68f65
Moe <emsfoit@gmail.com>
2022-03-22 16:40:46 +0100

    add dataset

---
+++ components/datasets/form.vue
@@ -0,0 +1,63 @@
+<template>
+  <b-modal
+    class="width"
+    size="lg"
+    :id="'dataset-form-modal'"
+    ref="dataset-modal"
+    title="Graph"
+  >
+    <div class="b-modal" style="max-height: 500px">
+      <form ref="form" @submit.stop.prevent="submit">
+        <b-form-group label="Datasets" label-for="dataset">
+          <b-form-file
+            v-model="form.files"
+            accept=".csv, .tsv"
+            id="dataset"
+            multiple
+            required
+          />
+        </b-form-group>
+        <b-form-group label="Separator" label-for="sep">
+          <b-form-input
+            id="sep"
+            v-model="form.sep"
+            placeholder="csv/tsv files separator"
+            required
+          />
+        </b-form-group>
+      </form>
+    </div>
+    <template #modal-footer>
+      <b-button size="sm" @click="toggleModal" variant="secondary">
+        Cancel
+      </b-button>
+      <b-button variant="primary" size="sm" @click="submit"> Submit </b-button>
+    </template>
+  </b-modal>
+</template>
+<script>
+export default {
+  name: "UploadButton",
+  data() {
+    return {
+      form: {
+        files: [],
+        sep: "",
+      },
+    };
+  },
+  methods: {
+    submit() {
+      if (this.form.sep && this.form.files.length > 0) {
+        debugger;
+        this.$emit("submitFiles", this.form.files, this.form.sep);
+        this.form.files = [];
+      }
+      return;
+    },
+    toggleModal() {
+      this.$refs["dataset-modal"].toggle("#toggle-btn");
+    },
+  },
+};
+</script>
--- components/datasets/list.vue
+++ components/datasets/list.vue
@@ -1,71 +1,32 @@
 <template>
-  <div>
-    <!-- <div class="row">
-      <search-panel v-if="is_company != true">
-        <span slot="title">{{ $t("helper.panel.search.dataset") }}</span>
-      </search-panel>
-    </div> -->
-    <datasets-upload @submitFiles="submitFiles" />
-
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Created At</th>
-          <th scope="col">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <datasets-single
-          v-for="dataset in datasets"
-          v-bind:dataset="dataset"
-          v-bind:key="dataset.id"
-        ></datasets-single>
-      </tbody>
-    </table>
-  </div>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Created At</th>
+        <th scope="col">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <datasets-single
+        v-for="dataset in datasets"
+        v-bind:dataset="dataset"
+        v-bind:key="dataset.id"
+      ></datasets-single>
+    </tbody>
+  </table>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-// import Search from "./search.vue";
-import { CREATE, FETCH_ALL } from "../../store/types/actions.type";
-import { SET_QUERIES, SET_LOADING } from "../../store/types/mutations.type";
 
 export default {
   name: "datasets_list",
-  methods: {
-    get_data() {
-      this.$store
-        .dispatch(`datasets/${FETCH_ALL}`, {
-          project_id: this.$route.params.id,
-        })
-        .then(() => {})
-        .catch((error) => {
-          alert(error);
-        });
-    },
-    submitFiles(files) {
-      debugger;
-      this.$store
-        .dispatch(`datasets/${CREATE}`, {
-          files: files,
-          project_id: this.$route.params.id,
-        })
-        .then(() => {})
-        .catch(() => {
-          alert("Error");
-        });
-    },
-  },
   computed: {
     ...mapGetters({
       datasets: "datasets/list",
     }),
-  },
-  created() {
-    this.get_data();
   },
 };
 </script>
