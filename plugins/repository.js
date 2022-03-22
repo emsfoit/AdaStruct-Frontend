4abac4e5ea2ab43fe9e91ced6bd07c62cb5edb4b
Moe <emsfoit@gmail.com>
2022-03-22 16:44:52 +0100

    inject API services

--- plugins/repository.js
+++ plugins/repository.js
@@ -1,11 +1,17 @@
 import AuthService from "../api/services/AuthService";
 import ProjectsService from "../api/services/ProjectsService";
 import DatasetsService from "../api/services/DatasetsService";
+import GraphsService from "../api/services/GraphsService";
+import LogsService from "../api/services/LogsService";
+import InferencesService from "../api/services/InferencesService";
 
 export default (ctx, inject) => {
   inject("AuthService", AuthService(ctx.$api));
   inject("ProjectsService", ProjectsService(ctx.$api));
   inject("DatasetsService", DatasetsService(ctx.$api));
+  inject("GraphsService", GraphsService(ctx.$api));
+  inject("LogsService", LogsService(ctx.$api));
+  inject("InferencesService", InferencesService(ctx.$api));
 
   // Add new api services here
 };
