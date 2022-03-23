4f8471eb3b346daa4bcfa7290a1cd56b17f858ff
Moe <emsfoit@gmail.com>
2022-03-23 20:08:34 +0100

    add chart component

---
+++ components/charts/training.vue
@@ -0,0 +1,61 @@
+<template>
+  <apexcharts
+    ref="demoChart"
+    type="area"
+    :options="chartOptions"
+    :series="series"
+    height="300"
+  />
+</template>
+<script>
+export default {
+  layout: "user",
+  name: "ChartsTraining",
+  props: {
+    series: {
+      type: Array,
+      default: () => [],
+    },
+    categories: {
+      type: Array,
+      default: () => [],
+    },
+    title: {
+      type: String,
+      default: () => "Chart",
+    },
+  },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: "line",
+          zoom: {
+            enabled: false,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: "straight",
+        },
+        title: {
+          text: this.title,
+          align: "left",
+        },
+        grid: {
+          row: {
+            colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+            opacity: 0.5,
+          },
+        },
+        xaxis: {
+          categories: this.categories,
+        },
+      },
+    };
+  },
+};
+</script>
