8c0a76f45142e22ac2fffcd5113b3e5ca2abe87d
Moe <emsfoit@gmail.com>
2022-03-23 21:12:55 +0100

    show charts in the log page

--- components/logs/show.vue
+++ components/logs/show.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="container">
+    <div class="row" v-if="log_extra_info.length > 0">
+      <charts-training
+        :series="lossSeries"
+        :categories="lossCategories"
+        class="col-md-6 col-12"
+        title="Training Loss"
+      />
+      <charts-training
+        :series="metricsSeries"
+        :categories="metricsCategories"
+        class="col-md-6 col-12"
+        title="Training Metrics"
+      />
+    </div>
     <div ref="logContainer" v-html="log_text" class="log-container" />
   </div>
 </template>
@@ -9,6 +23,14 @@ import { mapGetters } from "vuex";
 export default {
   layout: "user",
   name: "LogsShow",
+  data() {
+    return {
+      lossSeries: [],
+      metricsSeries: [],
+      lossCategories: [],
+      metricsCategories: [],
+    };
+  },
   computed: {
     ...mapGetters({
       log: "logs/one",
@@ -23,22 +45,79 @@ export default {
     },
     log_extra_info: {
       get: function () {
-        let sorted = {};
-        if (this.log && this.log.extra_info && this.log.extra_info.lengt > 0) {
-          let extra_info = this.log.extra_info;
-          for (elm in extra_info) {
-            sorted[elm.epoch][elm];
-          }
-          return this.log.log;
+        if (this.log && this.log.extra_info && this.log.extra_info.length > 0) {
+          let extra_info = this.log.extra_info.map((item) =>
+            JSON.parse(item.data)
+          );
+          return extra_info;
         }
-        return "";
+        return [];
       },
+    },
+    lossChartInfo() {
+      let log_extra_info = this.log_extra_info;
+      let items = log_extra_info.filter((item) => item.type == "training_loss");
+      let sorted_items = items.sort((a, b) => (a.epoch > b.epoch ? 1 : -1));
+      let data = sorted_items.map((item) => item.value.toFixed(3));
+      let categories = sorted_items.map((item) => item.epoch);
+      return {
+        series: [
+          {
+            name: "loss",
+            data: data,
+          },
+        ],
+        categories: categories,
+      };
+    },
+    metricChartInfo() {
+      let log_extra_info = this.log_extra_info;
+      let ndcg = [];
+      let mrr = [];
+      for (let elm of log_extra_info) {
+        if (elm.type == "NDCG") {
+          ndcg.push(elm);
+        } else if (elm.type == "MRR") {
+          mrr.push(elm);
+        }
+      }
+      // MRR
+      let sorted_mrr = mrr.sort((a, b) => (a.epoch > b.epoch ? 1 : -1));
+      let mrr_data = sorted_mrr.map((item) => item.value.toFixed(3));
+      // NDCG
+      let sorted_ndcg = ndcg.sort((a, b) => (a.epoch > b.epoch ? 1 : -1));
+      let ndcg_data = sorted_ndcg.map((item) => item.value.toFixed(3));
+      // SAME Categories
+      let categories = sorted_ndcg.map((item) => item.epoch);
+      return {
+        series: [
+          {
+            name: "NDCG",
+            data: ndcg_data,
+          },
+          {
+            name: "MRR",
+            data: mrr_data,
+          },
+        ],
+        categories: categories,
+      };
     },
   },
   updated() {
     let elm = this.$refs.logContainer;
     elm.scrollTop = elm.scrollHeight;
     elm.scrollIntoView({ behavior: "smooth" });
+    if (this.log_extra_info.length > 0) {
+      // SET Loss Chart
+      let data = this.lossChartInfo;
+      this.lossSeries = data.series;
+      this.lossCategories = data.categories;
+      // SET Metric Chart
+      let metrics_data = this.metricChartInfo;
+      this.metricsSeries = metrics_data.series;
+      this.metricsCategories = metrics_data.categories;
+    }
   },
 };
 </script>
